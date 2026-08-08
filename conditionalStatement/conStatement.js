d6db22a51967e4acc379fc09ede3562650692ddf
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-08-09 03:58:13 +0600

    conditional statement in js

---
+++ conditionalStatement/conStatement.js
@@ -0,0 +1,17 @@
+/*conditional statment use to controll the flow of program */
+//(if, else, else if, nested ife else, whitch, ternary)
+
+
+//if esle 
+let fristNumber  = 50;
+let secondNumber = 60;
+
+if(fristNumber > secondNumber) {
+    console.log(fristNumber);
+}else{
+    console.log(secondNumber);
+}
+
+
+//using ternary operator
+(fristNumber > secondNumber) ? console.log(fristNumber) : console.log(secondNumber);
