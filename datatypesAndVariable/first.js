71b97aacbb148c8bebd2ecceff2c65cc898c5cf4
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-08-09 00:10:25 +0600

    js variable

---
+++ datatypesAndVariable/first.js
@@ -0,0 +1,22 @@
+/*variable allows to store value so,
+ that we can leter or wheater needed in programm */
+
+/*datatypes tell the type of value stored in variable*/
+
+let name = "Rifat";//string datatypes
+let age = 25;//intiger datatypes
+let isMarrid = false;//boolean datatypes retuns 0 or 1 whre tree = 1 and false = 0
+let car;//undifined, we just decraled but not assigned any value to the car variable
+let jobPosition = null;//willingly assigned null,for assing value in future
+const pi = 3.1416//const keyword used to store only static value,that can not be change 
+
+console.log(name);
+console.log(age);
+console.log(isMarrid);
+console.log(car);
+console.log(jobPosition);
+console.log(pi);
+
+//typeof is an operator the gives types of data stored in variable
+console.log(typeof name);
+console.log(typeof age);
