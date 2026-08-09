5fdb10d3490fbaf438ac1555c6dfd99aef67f76e
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-08-09 22:57:16 +0600

    function in js

---
+++ function/function.js
@@ -0,0 +1,10 @@
+/* function is a named block of code,
+takes some input do some operaton and gives result*/
+
+function add(num1, num2) {//function declaration
+    return num1 + num2;
+}
+
+let sum = add(10,20);//function call
+console.log(sum);
+
